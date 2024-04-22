1139d42bcb247006d689731f12008cea78b18e23
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-21 21:38:42 -0300

    styles added

--- tp/tp-react/src/pages/Home/Home.jsx
+++ tp/tp-react/src/pages/Home/Home.jsx
@@ -1,97 +1,137 @@
-import Button from "../../components/Button/Button";
-import Title from '../../components/Title/Title';
 import { useState } from "react";
-import Input from "../../components/Input/Input";
-import style from "./Home.module.css";
+import {
+  Footer,
+  GenericButton,
+  InfoCard,
+  Input,
+  TaskFinder,
+  TaskTable,
+  TextArea,
+  Title,
+} from "../../components";
+import { Box, Grid } from "@mui/material";
 
 const Home = () => {
-
   const [actividades, setActividades] = useState([]);
 
   const [nuevaActividad, setNuevaActividad] = useState("");
+  const [descripcionActividad, setDescripcionActividad] = useState("");
 
   const [busqueda, setBusqueda] = useState("");
-  
-  const actividadesFiltradas = actividades.filter(actividad => actividad.name.toLowerCase().includes(busqueda.toLocaleLowerCase()));
 
-  const actividadesCompletas = actividades.filter(actividad => actividad.completada === true).length;
+  const actividadesFiltradas = actividades.filter((actividad) =>
+    actividad.name.toLowerCase().includes(busqueda.toLocaleLowerCase())
+  );
+
+  const actividadesCompletas = actividades.filter(
+    (actividad) => actividad.completada === true
+  ).length;
 
   const onChangeHandler = (event) => {
     setNuevaActividad(event.target.value);
-  }
+  };
+
+  const onChangeHandlerTextArea = (event) => {
+    setDescripcionActividad(event.target.value);
+  };
 
   const onChangeBusqueda = (event) => {
     setBusqueda(event.target.value);
-  }
+  };
 
   const guardarActividad = () => {
     const nuevaActividadAgregar = {
       name: nuevaActividad,
-      completada: false
+      description: descripcionActividad,
+      completada: false,
     };
     setActividades([...actividades, nuevaActividadAgregar]);
     setNuevaActividad("");
-  };
-
-  const eliminarActividad = (index) => {
-    const nuevaLista = [...actividades];
-    nuevaLista.splice(index, 1);
-    setActividades(nuevaLista);
-  };
-
-  const completarActividad = (index) => {
-    const actividadesActualizadas = [...actividades];
-    actividadesActualizadas[index].completada = !actividadesActualizadas[index].completada;
-    setActividades(actividadesActualizadas);
+    setDescripcionActividad("");
   };
 
   return (
-    <div className={style.home}>
-      <Title text="Aplicación: To-Do List" />      
+    <Box
+      sx={{
+        flexGrow: 1,
+        px: 3,
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        marginTop: "20px",
+      }}
+    >
+      <div style={{ marginBottom: "auto" }}>
+        <Grid container spacing={5} justifyContent="center" alignItems="center">
+          <Grid item xs={12}>
+            <Title text="Aplicación: To-Do List" />
+          </Grid>
+          <Grid item xs={12}>
+            <TaskFinder
+              value={busqueda}
+              onChangeHandler={onChangeBusqueda}
+              label="Buscar Tarea"
+            />
+          </Grid>
 
-      <div className={style.search}>
-        <div>
-            Buscar actividad:
-        </div>
-          <Input value={busqueda} onChangeHandler={onChangeBusqueda} placeholder="" />
-      </div>
-      
-    <div className={style.addtask}>
-      <Input value={nuevaActividad} onChangeHandler={onChangeHandler} placeholder="Escribe una actividad" />
-      <Button text="Agregar actividad" onClick={guardarActividad} disabled={nuevaActividad ? "" : "disabled"} />
-    </div>
+          <Grid item xs={4}>
+            <Box
+              sx={{
+                "& > :not(style)": { m: 1 },
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Input
+                value={nuevaActividad}
+                onChangeHandler={onChangeHandler}
+                label="Escribe una actividad"
+              />
+              <TextArea
+                value={descripcionActividad}
+                onChangeHandler={onChangeHandlerTextArea}
+                label="Descripcion"
+              />
+              <GenericButton
+                text="Agregar actividad"
+                onClick={guardarActividad}
+                disabled={nuevaActividad && descripcionActividad ? false : true}
+              />
+            </Box>
+          </Grid>
 
-    <div className = {style.tasklist}>
-      {actividades.length === 0 ? ( 
-        <p>No hay actividades para realizar</p>
-      ) : (
-        actividadesFiltradas.length === 0 ? (
-          <p>No se encontraron coincidencias</p>
-        ) : (
-          actividadesFiltradas.map((actividad, index) => (
-            <div className = {style.individualTask} key = {index}>
-               <div style={{ textDecoration: actividad.completada ? 'line-through' : 'none' }} key={actividad.descripcion}>
-                  <p>{actividad.name}</p>
-                </div>
-                <div>
-                  <Button text={actividad.completada ? "Desmarcar actividad" : "Marcar como Completa"} onClick={() => completarActividad(index)}/>
-                  <Button text="Eliminar actividad" onClick={() => eliminarActividad(index)}  />
-                </div>
-            </div>
-          ))
-        )
-      )}
-    </div>
+          <Grid item xs={8}>
+            {actividades.length === 0 ? (
+              <InfoCard
+                text="No hay actividades para realizar"
+                textColor="#111111"
+                borderColor="#200583"
+              />
+            ) : actividadesFiltradas.length === 0 ? (
+              <InfoCard
+                text="No se encontraron coincidencias"
+                textColor="#E91E63"
+                borderColor="#E91E63"
+              />
+            ) : (
+              <TaskTable
+                task={actividadesFiltradas}
+                setActividades={setActividades}
+              />
+            )}
+          </Grid>
 
-    <div className = {style.counter}>
-      <div>
-        <p>Actividades Totales:</p> {actividades.length}
+          <Grid item xs={6}>
+            <InfoCard
+              text={`Actividades Totales: ${actividades.length} - Actividades Completadas: ${actividadesCompletas}`}
+            />
+          </Grid>
+        </Grid>
       </div>
-      <div>
-        <p>Actividades Completadas:</p> {actividadesCompletas}
-      </div>
-    </div>
-  </div>
+      <Footer />
+    </Box>
   );
 };
 
