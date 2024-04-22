f18ac40c0e8d06632d4dbb32e2b69d9aab18d9b1
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-21 21:33:25 -0300

    footer component

---
+++ tp/tp-react/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material";
+import { SubTitle } from "../SubTitle/SubTitle";
+
+export const Footer = () => (
+  <footer
+    style={{
+      padding: "20px",
+      borderTop: "1px solid #ccc",
+      marginTop: "10px",
+    }}
+  >
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+      <Grid item xs={8}>
+        <SubTitle text="Desarrollado por Centena Jose, Diaz Fabricio, Nacho Cayuqueo" />
+        <SubTitle text="Grupo 6 - Programación Web Avanzada" />
+      </Grid>
+    </Grid>
+  </footer>
+);
